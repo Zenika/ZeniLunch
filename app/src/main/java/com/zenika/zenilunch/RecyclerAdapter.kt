170367eb8f1e4a37c798aca881391092a1b88cbc
Audrey Gentili <audrey.gentili@zenika.com>
2023-03-09 09:54:07 +0100

    Update nommage et styling

--- app/src/main/java/com/zenika/zenilunch/RecyclerAdapter.kt
+++ app/src/main/java/com/zenika/zenilunch/RecyclerAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListAdapter(private val list: List<ItemView>): RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+class RecyclerAdapter(private val restaurants: List<RestaurantUIModel>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -14,18 +14,18 @@ class ListAdapter(private val list: List<ItemView>): RecyclerView.Adapter<ListAd
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ListAdapter.ViewHolder, position: Int) {
-        val itemsView = list[position]
-        //holder.id.text = ItemView.id.toString()
-        holder.name.text = itemsView.name
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        val RestaurantUIModel = restaurants[position]
+        //holder.id.text = RestaurantUIModel.id.toString()
+        holder.nom.text = RestaurantUIModel.nom
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return restaurants.size
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //val id: TextView = itemView.findViewById(R.id.id)
-        val name: TextView = itemView.findViewById(R.id.name)
+        val nom: TextView = itemView.findViewById(R.id.nom)
     }
 }
