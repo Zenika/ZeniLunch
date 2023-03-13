f3c78fb1c8c5a0fab77982863093cdf87994b22b
Audrey Gentili <audrey.gentili@zenika.com>
2023-03-13 16:43:26 +0100

    Restaurants list

--- app/src/main/java/com/zenika/zenilunch/MainActivity.kt
+++ app/src/main/java/com/zenika/zenilunch/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.zenika.zenilunch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = RestaurantAdapter { restaurant ->
+            Toast.makeText(this, restaurant.name, Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<RecyclerView>(R.id.recyclerview).apply {
+            this.layoutManager = LinearLayoutManager(this@MainActivity)
+            this.adapter = adapter
+        }
+
+        val restaurants = createRestaurants()
+        adapter.submitList(restaurants)
+    }
+
+    private fun createRestaurants(): List<RestaurantUIModel> {
+        return listOf(
+            RestaurantUIModel("Kaffee Berlin"),
+            RestaurantUIModel("Happy Feel"),
+            RestaurantUIModel("Chez Jules"),
+            RestaurantUIModel("O Pad Thaï"),
+            RestaurantUIModel("Jojo Pizza")
+        )
     }
 }
---
+++ app/src/main/java/com/zenika/zenilunch/RestaurantUIModel.kt
@@ -0,0 +1,9 @@
+package com.zenika.zenilunch
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class RestaurantUIModel(
+    val name: String
+) : Parcelable
