cf8ee46b70249b73c829a213298ce47354439a93
Audrey Gentili <audrey.gentili@zenika.com>
2023-03-14 10:59:51 +0100

    Add Google Maps link button

--- app/src/main/java/com/zenika/zenilunch/DetailActivity.kt
+++ app/src/main/java/com/zenika/zenilunch/DetailActivity.kt
@@ -1,5 +1,7 @@
 package com.zenika.zenilunch
 
+import android.content.Intent
+import android.net.Uri
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -22,7 +24,10 @@ class DetailActivity : AppCompatActivity() {
 
         intent.extras
             ?.getParcelable<RestaurantUIModel>("restaurant")
-            ?.let { restaurant -> displayDetails(restaurant) }
+            ?.let { restaurant ->
+                displayDetails(restaurant)
+                getAddress(restaurant)
+            }
             ?: error("This activity requires a restaurant!")
     }
 
@@ -39,6 +44,20 @@ class DetailActivity : AppCompatActivity() {
             restaurant.vegan -> getString(R.string.option, getString(R.string.vegan))
             restaurant.vegetarian -> getString(R.string.option, getString(R.string.vegetarian))
             else -> getString(R.string.noOption)
+        }
+    }
+
+    private fun getAddress(restaurant: RestaurantUIModel) {
+        val address = findViewById<TextView>(R.id.map)
+        address?.setOnClickListener () {
+            val officeLatitude = 45.766752337134754
+            val officeLongitude = 4.858952442403011
+            val latitude = restaurant.latitude
+            val longitude = restaurant.longitude
+            val url = "http://maps.google.com/maps?saddr=$officeLatitude,$officeLongitude&daddr=$latitude,$longitude"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            intent.setPackage("com.google.android.apps.maps")
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/zenika/zenilunch/MainActivity.kt
+++ app/src/main/java/com/zenika/zenilunch/MainActivity.kt
@@ -26,11 +26,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun createRestaurants(): List<RestaurantUIModel> {
         return listOf(
-            RestaurantUIModel("Kaffee Berlin", "Burger", "€", vegetarian = true, vegan = true),
-            RestaurantUIModel("Happy Feel", "Végétarien", "€", vegetarian = true, vegan = true),
-            RestaurantUIModel("Chez Jules", "Boulangerie", "€", vegetarian = false, vegan = false),
-            RestaurantUIModel("O Pad Thaï", "Thaïlandais", "€", vegetarian = false, vegan = false),
-            RestaurantUIModel("Jojo Pizza", "Pizzeria", "€", vegetarian = true, vegan = false)
+            RestaurantUIModel("Kaffee Berlin", "Burger", "€", vegetarian = false, vegan = false, 45.767551889608235, 4.857335592897319),
+            RestaurantUIModel("Happy Feel", "Végétarien", "€", vegetarian = true, vegan = true, 45.76864100678723, 4.8619654828776016),
+            RestaurantUIModel("Chez Jules", "Boulangerie", "€", vegetarian = false, vegan = false, 45.76648502742043, 4.856709398222179),
+            RestaurantUIModel("O Pad Thaï", "Thaïlandais", "€", vegetarian = false, vegan = false, 45.76399480415859, 4.856358936709006),
+            RestaurantUIModel("Jojo Pizza", "Pizzeria", "€", vegetarian = true, vegan = false, 45.77004280935307, 4.858317698222286)
         )
     }
 }
