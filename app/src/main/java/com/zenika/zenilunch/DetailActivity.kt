794bbe9a73243ae84f989a424f29c833dd634e66
Audrey Gentili <audrey.gentili@zenika.com>
2023-03-15 09:54:28 +0100

    Update restaurants details activity

--- app/src/main/java/com/zenika/zenilunch/DetailActivity.kt
+++ app/src/main/java/com/zenika/zenilunch/DetailActivity.kt
@@ -1,31 +1,22 @@
 package com.zenika.zenilunch
 
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_activity)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val bundleRestaurant = intent.extras
-        val restaurant: RestaurantUIModel
-
-        if (bundleRestaurant != null) {
-            restaurant = bundleRestaurant.getParcelable("restaurant", RestaurantUIModel::class.java)!!
-            getInfo(restaurant)
-        } else {
-            Log.e("DetailActivity", "RestaurantUIModel object error")
-        }
+        intent.extras
+            ?.getParcelable<RestaurantUIModel>("restaurant")
+            ?.let { restaurant -> displayDetails(restaurant) }
+            ?: error("This activity requires a restaurant!")
     }
 
-    private fun getInfo(restaurant: RestaurantUIModel) {
+    private fun displayDetails(restaurant: RestaurantUIModel) {
         val name = findViewById<TextView>(R.id.name)
         val type = findViewById<TextView>(R.id.type)
         val price = findViewById<TextView>(R.id.price)
@@ -34,10 +25,14 @@ class DetailActivity : AppCompatActivity() {
         type.text = restaurant.type
         price.text = restaurant.price
 
-        option.text = ""
-        if (restaurant.vegetarian && restaurant.vegan) {
-            option.text = "Options : Végétarien & Vegan"
-        } else if (restaurant.vegetarian) option.text = "Option : Végétarien"
+        val optionValue = when {
+            !restaurant.vegetarian -> "Non végétarien"
+            restaurant.vegetarian && !restaurant.vegan-> "Végétarien"
+            restaurant.vegan -> "Végétarien et vegan"
+            else -> "Pas d'option"
+        }
+
+        option.append(optionValue)
     }
 
     override fun onSupportNavigateUp(): Boolean {
