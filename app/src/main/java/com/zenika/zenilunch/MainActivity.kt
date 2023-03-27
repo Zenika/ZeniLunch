f0ee26d73a2b1a160b5695f814a8db2b243d29c6
Audrey Gentili <audrey.gentili@zenika.com>
2023-03-27 10:02:31 +0200

    Update with Navigation

--- app/src/main/java/com/zenika/zenilunch/MainActivity.kt
+++ app/src/main/java/com/zenika/zenilunch/MainActivity.kt
@@ -1,36 +1,57 @@
 package com.zenika.zenilunch
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
+var modifier = Modifier
+    .fillMaxWidth()
+    .padding(10.dp)
+
+@OptIn(ExperimentalAnimationApi::class)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val adapter = RestaurantAdapter { restaurant ->
-            val intent = DetailActivity.getStartIntent(this, restaurant)
-            startActivity(intent)
+        setContent {
+            MyAppNavHost()
         }
-
-        findViewById<RecyclerView>(R.id.recyclerview).apply {
-            this.layoutManager = LinearLayoutManager(this@MainActivity)
-            this.adapter = adapter
-        }
-
-        val restaurants = createRestaurants()
-        adapter.submitList(restaurants)
     }
 
-    private fun createRestaurants(): List<RestaurantUIModel> {
-        return listOf(
-            RestaurantUIModel("Kaffee Berlin", "Burger", "€", vegetarian = true, vegan = true, 45.767551889608235, 4.857335592897319),
-            RestaurantUIModel("Happy Feel", "Végétarien", "€", vegetarian = true, vegan = true, 45.76864100678723, 4.8619654828776016),
-            RestaurantUIModel("Chez Jules", "Boulangerie", "€", vegetarian = false, vegan = false, 45.76648502742043, 4.856709398222179),
-            RestaurantUIModel("O Pad Thaï", "Thaïlandais", "€", vegetarian = false, vegan = false, 45.76399480415859, 4.856358936709006),
-            RestaurantUIModel("Jojo Pizza", "Pizzeria", "€", vegetarian = true, vegan = false, 45.77004280935307, 4.858317698222286)
-        )
+    @Composable
+    fun MyAppNavHost(
+        modifier: Modifier = Modifier,
+        navController: NavHostController = rememberAnimatedNavController(),
+        startDestination: String = "list"
+    ) {
+        AnimatedNavHost(
+            modifier = modifier,
+            navController = navController,
+            startDestination = startDestination
+        ) {
+            composable("list") {
+                ListScreen { restaurant ->
+                    val name = restaurant.name
+                    navController.navigate("detail/$name")
+                }
+            }
+            composable("detail/{name}", arguments = listOf(navArgument("name") {
+                type = NavType.StringType
+            })) { backStackEntry ->
+                val name = backStackEntry.arguments?.getString("name")
+                DetailScreen()
+            }
+        }
     }
 }
