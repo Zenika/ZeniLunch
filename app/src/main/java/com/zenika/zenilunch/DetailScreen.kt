d705f4773747c487b9585af5eac68be0b93ed875
Audrey Gentili <audrey.gentili@zenika.com>
2023-03-27 10:52:55 +0200

    Add stringResource for option values

--- app/src/main/java/com/zenika/zenilunch/DetailScreen.kt
+++ app/src/main/java/com/zenika/zenilunch/DetailScreen.kt
@@ -8,14 +8,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun DetailScreen(viewModel: DetailViewModel = hiltViewModel()) {
-    viewModel.setRestaurantName("Kaffee Berlin")
     val restaurant = viewModel.getRestaurant()
+    val option = when {
+        restaurant.vegan -> stringResource(R.string.option, stringResource(R.string.vegan))
+        restaurant.vegetarian -> stringResource(R.string.option, stringResource(R.string.vegetarian))
+        else -> stringResource(R.string.noOption)
+    }
     Column(
         Modifier
             .fillMaxWidth()
@@ -30,7 +35,7 @@ fun DetailScreen(viewModel: DetailViewModel = hiltViewModel()) {
         Text(text = restaurant.price, modifier,
             textAlign = TextAlign.Center,
             fontSize = 20.sp)
-        Text(text = "Option", modifier,
+        Text(text = option, modifier,
             textAlign = TextAlign.Center,
             fontSize = 18.sp)
         Button(onClick = { viewModel.openGoogleMaps() }) {
