40d0c3a17bdfa6362cfcbcbabf4b067af0562cc9
Audrey Gentili <audrey.gentili@zenika.com>
2023-03-30 17:37:11 +0200

    Change composables padding

--- app/src/main/java/com/zenika/zenilunch/ListScreen.kt
+++ app/src/main/java/com/zenika/zenilunch/ListScreen.kt
@@ -2,8 +2,11 @@ package com.zenika.zenilunch
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,14 +15,15 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -43,7 +47,11 @@ fun ListScreen(
         content = { innerPadding ->
             Column(
                 Modifier
-                    .padding(innerPadding)
+                    .padding(
+                        top = innerPadding.calculateTopPadding(),
+                        start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
+                        end = innerPadding.calculateEndPadding(LocalLayoutDirection.current)
+                    )
             ) {
                 Text(
                     text = stringResource(R.string.description),
@@ -56,10 +64,10 @@ fun ListScreen(
                     verticalArrangement = Arrangement.spacedBy(16.dp),
                     horizontalAlignment = CenterHorizontally,
                     contentPadding = PaddingValues(
-                        screenPadding,
-                        screenPadding,
-                        screenPadding,
-                        70.dp
+                        start = screenPadding,
+                        top = screenPadding,
+                        end = screenPadding,
+                        bottom = screenPadding + innerPadding.calculateBottomPadding(),
                     ),
                     state = state
                 ) {
@@ -85,18 +93,20 @@ fun ListScreen(
                 }
             }
         },
-        floatingActionButtonPosition = FabPosition.Center,
-        floatingActionButton = {
-            Button(
-                onClick = {
-                    val randomIndex = Random.nextInt(restaurants.size)
-                    val restaurant = restaurants[randomIndex]
-                    onSuggestionClick(restaurant)
-                }) {
-                Text(
-                    text = stringResource(R.string.suggestion),
-                    fontSize = 20.sp
-                )
+        bottomBar = {
+            Box(Modifier.fillMaxWidth()) {
+                Button(
+                    modifier = Modifier.align(BottomCenter).padding(screenPadding),
+                    onClick = {
+                        val randomIndex = Random.nextInt(restaurants.size)
+                        val restaurant = restaurants[randomIndex]
+                        onSuggestionClick(restaurant)
+                    }) {
+                    Text(
+                        text = stringResource(R.string.suggestion),
+                        fontSize = 20.sp
+                    )
+                }
             }
         }
     )
@@ -104,15 +114,95 @@ fun ListScreen(
 
 private fun createRestaurants(): List<RestaurantUIModel> {
     return listOf(
-        RestaurantUIModel("Kaffee Berlin", "Burger", "€€", vegetarian = true, vegan = true, 45.767551889608235, 4.857335592897319),
-        RestaurantUIModel("Happy Feel", "Végétarien", "€€", vegetarian = true, vegan = true, 45.76864100678723, 4.8619654828776016),
-        RestaurantUIModel("Chez Jules", "Boulangerie", "€", vegetarian = false, vegan = false, 45.76648502742043, 4.856709398222179),
-        RestaurantUIModel("O Pad Thaï", "Thaïlandais", "€€", vegetarian = false, vegan = false, 45.76399480415859, 4.856358936709006),
-        RestaurantUIModel("Jojo Pizza", "Pizzeria", "€", vegetarian = true, vegan = false, 45.77004280935307, 4.858317698222286),
-        RestaurantUIModel("Ok Sushi", "Japonais", "€€", true, false, 45.76855096798334, 4.8494913730169795),
-        RestaurantUIModel("Chikin Bang", "Coréen", "€", false, false, 45.761726612268745, 4.856067793275381),
-        RestaurantUIModel("Manger Vite & Bien", "Bar à salades", "€", true, true, 45.764033379006165, 4.857152298222151),
-        RestaurantUIModel("Subway", "Sandwich", "€€", true, true, 45.76959922404694, 4.854517083590193),
-        RestaurantUIModel("Pepe Pizza", "Pizzeria", "€€", false, false, 45.76609019074048, 4.854910608843241)
+        RestaurantUIModel(
+            "Kaffee Berlin",
+            "Burger",
+            "€€",
+            vegetarian = true,
+            vegan = true,
+            45.767551889608235,
+            4.857335592897319
+        ),
+        RestaurantUIModel(
+            "Happy Feel",
+            "Végétarien",
+            "€€",
+            vegetarian = true,
+            vegan = true,
+            45.76864100678723,
+            4.8619654828776016
+        ),
+        RestaurantUIModel(
+            "Chez Jules",
+            "Boulangerie",
+            "€",
+            vegetarian = false,
+            vegan = false,
+            45.76648502742043,
+            4.856709398222179
+        ),
+        RestaurantUIModel(
+            "O Pad Thaï",
+            "Thaïlandais",
+            "€€",
+            vegetarian = false,
+            vegan = false,
+            45.76399480415859,
+            4.856358936709006
+        ),
+        RestaurantUIModel(
+            "Jojo Pizza",
+            "Pizzeria",
+            "€",
+            vegetarian = true,
+            vegan = false,
+            45.77004280935307,
+            4.858317698222286
+        ),
+        RestaurantUIModel(
+            "Ok Sushi",
+            "Japonais",
+            "€€",
+            true,
+            false,
+            45.76855096798334,
+            4.8494913730169795
+        ),
+        RestaurantUIModel(
+            "Chikin Bang",
+            "Coréen",
+            "€",
+            false,
+            false,
+            45.761726612268745,
+            4.856067793275381
+        ),
+        RestaurantUIModel(
+            "Manger Vite & Bien",
+            "Bar à salades",
+            "€",
+            true,
+            true,
+            45.764033379006165,
+            4.857152298222151
+        ),
+        RestaurantUIModel(
+            "Subway",
+            "Sandwich",
+            "€€",
+            true,
+            true,
+            45.76959922404694,
+            4.854517083590193
+        ),
+        RestaurantUIModel(
+            "Pepe Pizza",
+            "Pizzeria",
+            "€€",
+            false,
+            false,
+            45.76609019074048,
+            4.854910608843241
+        )
     )
 }
